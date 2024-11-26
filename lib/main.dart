2f82073c5244a74363985fad774f131428fe1407
Alarcon Steven <academicacoordinacionguaviare@gmail.com>
2024-11-26 14:59:53 -0500

    inicio de generación de apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cdattg_sena_mobile/config/routes/router_app.dart';
 import 'package:cdattg_sena_mobile/config/theme/theme_app.dart';
 import 'package:cdattg_sena_mobile/features/auth/domain/domain.dart';
+import 'package:cdattg_sena_mobile/home/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -16,22 +17,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: authService.isAuthenticated(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator(); // Mostrar un indicador de carga mientras se verifica la autenticación
-        } else {
-          final bool isLoggedIn = snapshot.data ?? false;
-          final routerConfig = isLoggedIn ? routerApp : routerApp;
+    //   return FutureBuilder<bool>(
+    //     future: authService.isAuthenticated(),
+    //     builder: (context, snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.waiting) {
+    //         return const CircularProgressIndicator(); // Mostrar un indicador de carga mientras se verifica la autenticación
+    //       } else {
+    //         final bool isLoggedIn = snapshot.data ?? false;
+    //         final routerConfig = isLoggedIn ? routerApp : routerApp;
 
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            routerConfig: routerConfig,
-            theme: ThemeApp(colorSelector: 0).themeData,
-          );
-        }
-      },
+    //         return MaterialApp.router(
+    //           debugShowCheckedModeBanner: false,
+    //           routerConfig: routerConfig,
+    //           theme: ThemeApp(colorSelector: 0).themeData,
+    //         );
+    //       }
+    //     },
+    //   );
+    // }
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: routerApp,
+      theme: ThemeApp(colorSelector: 0).themeData,
     );
   }
 }
